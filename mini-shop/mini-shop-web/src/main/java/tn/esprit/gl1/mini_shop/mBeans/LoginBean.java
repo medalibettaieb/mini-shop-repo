1712d9077ff813ef80aafb238177670b6dbdef58
bettaieb med ali <medali.bettaieb@esprit.tn>
2015-11-16 12:25:08 +0100

    real login
    Signed-off-by: bettaieb med ali <medali.bettaieb@esprit.tn>

--- mini-shop/mini-shop-web/src/main/java/tn/esprit/gl1/mini_shop/mBeans/LoginBean.java
+++ mini-shop/mini-shop-web/src/main/java/tn/esprit/gl1/mini_shop/mBeans/LoginBean.java
@@ -1,34 +1,44 @@
 package tn.esprit.gl1.mini_shop.mBeans;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import tn.esprit.gl1.mini_shop.persistence.Admin;
+import tn.esprit.gl1.mini_shop.persistence.Customer;
+import tn.esprit.gl1.mini_shop.persistence.User;
+import tn.esprit.gl1.mini_shop.services.UserServiceLocal;
 
 @ManagedBean
 @SessionScoped
 public class LoginBean {
-	private String login;
-	private String password;
+	// Model
+	private User user = new User();
+	// injection of the proxy
+	@EJB
+	private UserServiceLocal userServiceLocal;
 
+	// Methods
 	public String doLogin() {
-		System.out.println("the login is : " + login);
-		System.out.println("the pwd is : " + password);
+		User userLoggedIn = userServiceLocal.authenticate(user.getLogin(),
+				user.getPassword());
+		if (userLoggedIn instanceof Admin) {
+			System.out.println("welcom Admin");
+		} else if (userLoggedIn instanceof Customer) {
+			System.out.println("welcom customer");
+		} else {
+			System.out.println("rawa7 ldarkom ...");
+		}
+
 		return "";
 	}
 
-	public String getLogin() {
-		return login;
+	public User getUser() {
+		return user;
 	}
 
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 }
